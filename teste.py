8df381ecb878513f087d873b70d52229f19dec11
Gabriel Moraes <gcmorais66@gmail.com>
2024-02-15 21:30:34 -0300

    Fix

--- teste.py
+++ teste.py
@@ -379,12 +379,19 @@ def order_packages():
         except Exception as e:
             print("Erro ao ordenar pacotes: ", e)
 
-def order_messages(messages):
-    key_function = lambda x: (eval(x['message_id'][1]), eval(x['message_id'][0]))
+def key_function(message):
+    message_id = message['message_id']
+    second_value = message_id[1] if isinstance(message_id, tuple) and len(message_id) >= 2 else 0
+    first_value = message_id[0] if isinstance(message_id, tuple) else 0
+    return (second_value, first_value)
 
-    # Ordena a lista com base no segundo valor de 'message_id' e, em caso de empate, no primeiro valor
-    sorted_messages = sorted(messages, key=key_function)
-    return sorted_messages
+def order_messages(messages):
+    try:
+        sorted_messages = sorted(messages, key=key_function)
+        return sorted_messages
+    except Exception as e:
+        print("Erro ao ordenar mensagens:", e)
+        return []
 
 def read_messages():
     
